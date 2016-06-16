9c70f02fcf0735c2997373060a963301e7fbb7f8
SmoDav <smodavprivate@gmail.com>
2016-06-16 09:45:53 +0300

    publishing assets and readme

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,38 +1,32 @@
 @if(session()->has('sf_persist'))
     <script>
-        swal({
+        sflash({
             title: "{{ session('sf_title') }}",
             text: "{{ session('sf_message') }}",
             type: "{{ session('sf_level') }}",
             allowOutsideClick: true,
-            confirmButtonText: "Got it"
+            confirmButtonText: "Got it",
+            showConfirmButton: true
         });
     </script>
 @elseif(session()->has('sf_message'))
     <script>
-        swal({
+        sflash({
             title: "{{ session('sf_title') }}",
             text: "{{ session('sf_message') }}",
             type: "{{ session('sf_level') }}",
             allowOutsideClick: true,
-            timer: 2500,
-            showConfirmButton: false
+            showConfirmButton: false,
+            timer: 2500
         });
     </script>
 @endif
-
 @if(session()->has('sf_notice'))
-    <div class="sf_notice">
-        <span class="sf_notice_body">{{ session('sf_notice_message') }}.</span>
-    </div>
-
-    <script id="notice_script">
-        $(".sf_notice").animate({ left: '-10px' })
-                .delay(2000)
-                .animate({ left: '-500px' });
-        setTimeout(function () {
-            $(".sf_notice").remove();
-        }, 3000);
-        $("#notice_script").remove();
+    <script>
+        sflash({
+            type: 'notice',
+            message: '{{ session('sf_notice_message') }}',
+            timer: 3000
+        });
     </script>
 @endif
